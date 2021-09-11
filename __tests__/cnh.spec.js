ac1faddca347d7404051bddf2512d0141bf018bc
jjullimayanne <julli.souza@ufob.edu.br>
2021-09-11 18:11:37 -0300

    teste

--- __tests__/cnh.spec.js
+++ __tests__/cnh.spec.js
@@ -3,3 +3,11 @@ const podeTirarCarteiraDeMotorista = require('../src/cnh')
 test('Deve retornar false para pessoa com menos de 18 anos', () => {
   expect(podeTirarCarteiraDeMotorista(17)).toBe(false)
 })
+test('Deve retornar true para pessoa com mais de 18 anos', () => {
+  expect(podeTirarCarteiraDeMotorista(19)).toBe(true)
+})
+
+test('Deve retornar true para pessoa com mais de 18 anos', () => {
+  expect(podeTirarCarteiraDeMotorista(18)).toBe(true)
+})
+
